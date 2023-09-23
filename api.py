28aa9c3a88e48051254072338e1732d02923d84f
Ha__sh <anelegistfrom98@yahoo.com>
2023-09-23 12:45:08 +0530

    Update api.py

--- api.py
+++ api.py
@@ -63,7 +63,7 @@ def upload(pdf):
         "s3",
         # aws_access_key_id=ACCESS_KEY,
         # aws_secret_access_key=SECRET_KEY,
-        # aws_session_token=SESSION_TOKEN
+        # region=REGION
                       )
     # Downloading a csv file 
     # from S3 bucket to local folder
